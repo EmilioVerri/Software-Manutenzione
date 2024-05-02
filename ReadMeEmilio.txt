5e6ae3e2e2ca0830128156e8ae5633ce8d965af4
EmilioVerri <emilioverri83@gmail.com>
2024-05-02 15:26:44 +0200

    Primo Commit

---
+++ ReadMeEmilio.txt
@@ -0,0 +1,2 @@
+Ricordati di installare sul computer in cui andrai a testare questo file:
+https://www.microsoft.com/en-us/download/confirmation.aspx?id=50420
---
+++ www/index.php
@@ -0,0 +1,53 @@
+<?php
+/*
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+    
+
+    if (!isset($_SESSION['FirstVisit'])) {
+
+    //show site for the first time part
+    $_SESSION['FirstVisit'] = 1;
+ 
+
+    // Don't forget to add http colon slash slash www dot before!
+
+    } else { 
+        $_SESSION['FirstVisit']++;
+     }
+*/
+?>
+
+
+<html>
+<head>
+<title>Baxter Application</title>
+<link rel="stylesheet" type="text/css" href="/uikit-3.15.9/css/uikit-rtl.css">
+<link rel="stylesheet" type="text/css" href="/uikit-3.15.9/css/uikit-rtl.min.css">
+<link rel="stylesheet" type="text/css" href="/uikit-3.15.9/css/uikit.css">
+<link rel="stylesheet" type="text/css" href="/uikit-3.15.9/css/uikit.min.css">
+<link rel="stylesheet" type="text/css" href="/uikit-3.15.9/js/uikit-icons.js">
+<link rel="stylesheet" type="text/css" href="/uikit-3.15.9/js/uikit-icons.min.js">
+<link rel="stylesheet" type="text/css" href="/uikit-3.15.9/js/uikit.js">
+<link rel="stylesheet" type="text/css" href="/uikit-3.15.9/js/uikit.min.js">
+
+
+
+
+</head>
+<body>
+
+</body>
+
+
+</html>
+
+
+
+
+
+
+
